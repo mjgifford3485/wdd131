59d412d1b7b5935aea2c38a4f09097e84e3dd977
mjgifford3485 <mjgifford3485@gmail.com>
2025-03-15 14:11:57 -0700

    temple album project

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+const lastModified = document.lastModified;
+
+document.getElementById("lastModified").textContent = lastModified;
+
+const year = document.querySelector("#currentyear");
+
+const today = new Date();
+
+year.innerHTML = today.getFullYear();
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
